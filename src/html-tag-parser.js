63698b2ba5bd1f17284776f675587b15a8d1908f
asthadixit <astha.dixit@goibibo.com>
2017-08-25 00:50:03 +0530

    HTML Parser for parsing any HTML string

---
+++ src/html-tag-parser.js
@@ -0,0 +1,27 @@
+
+var HtmlParser = function(textToParse, tagToPick) {
+	var openTag = tagToPick;
+  var closetag = tagToPick[0] + '/' + tagToPick.substr(1);
+  var splitArray = text.split(openTag);
+  var textArray = [];
+  var textObject = [];
+
+  if (splitArray[0].indexOf(closetag) === -1) {
+    textObject.startText = splitArray[0];
+    splitArray.shift();
+  } else {
+    textObject.startText = '';
+  }
+
+  splitArray.forEach(function(txt, ind) {
+    var secondSplit = txt.split(closetag);
+    var tempObj = {};
+    tempObj.msg = secondSplit[0] || '';
+    tempObj.postText = secondSplit[1] || '';
+    textArray.push(tempObj);
+  });
+
+  textObject.textArray = textArray;
+
+	return textObject;
+}
